2d2c3075cd69b1c96995d6f694f317bfe471e5a8
arkantrust <davidulce26@gmail.com>
2023-12-04 11:06:27 -0500

    Added tests

--- packages/graphs/test/graphs_test.dart
+++ packages/graphs/test/graphs_test.dart
@@ -3,21 +3,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:graphs/graphs.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+
 
   test('Test Edge class', () {
-    final Edge<int> edge = Edge(weight: 1);
+    const Edge<int> edge = Edge(weight: 1);
     expect(edge.weight, 1);
     expect(edge.copyWith(weight: 2).weight, 2);
   });
 
   test('Test Node class', () {
-    final Node<int> node = Node(data: 1000);
+    const Node<int> node = Node(data: 1000);
     expect(node.data, 1000);
     expect(node.copyWith(data: 2000).data, 2000);
   });
